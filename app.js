6375baaeaf23d522bbc28cdef469f1a6460787dc
ZabavchukBogdan <117076795+ZabavchukBogdan@users.noreply.github.com>
2023-08-17 15:13:47 +0300

    HM 3 node.js

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 
-const contactsRouter = require('./routes/api/contacts')
+const contactsRouter = require("./routes/api/contacts");
+
+require("dotenv").config();
+
 
 const app = express()
 
--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-const app = require('./app')
+const app = require('./app');
+const mongoose = require("mongoose");
+const { DB_HOST = "mongodb+srv://bogdan:qazwsxedc@cluster0.weuixzx.mongodb.net/db-contacts", PORT = 3000 } = process.env;
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+
+
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
