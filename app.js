a1a4810a06a23889e994779e4600d5cc7077c4af
ZabavchukBogdan <117076795+ZabavchukBogdan@users.noreply.github.com>
2023-08-27 19:32:02 +0300

    HM 4 node.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const logger = require('morgan')
 const cors = require('cors')
 
 const contactsRouter = require("./routes/api/contacts");
-
+const userRouter = require("./routes/api/auth");
 require("dotenv").config();
 
 
@@ -16,6 +16,8 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
+app.use("/api/users", userRouter);
+
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,19 +1,24 @@
 const express = require("express");
-const router = express.Router();
 const ctrl = require("../../controllers/contacts");
-const { validateBody, isValidId } = require("../../middlewares");
+const { validateBody, isValidId, authenticate } = require("../../middlewares");
 const { schemas } = require("../../models/contacts");
 
+const router = express.Router();
 
+router.get("/", authenticate, ctrl.getAllContacts);
 
-router.get("/", ctrl.getAllContacts);
+router.get("/:contactId", authenticate, isValidId, ctrl.getContactById);
 
-router.get("/:contactId", isValidId, ctrl.getContactById);
-
-router.post("/", validateBody(schemas.addSchema), ctrl.addContact);
+router.post(
+  "/",
+  authenticate,
+  validateBody(schemas.addSchema),
+  ctrl.addContact
+);
 
 router.put(
   "/:contactId",
+  authenticate,
   isValidId,
   validateBody(schemas.addSchema),
   ctrl.updateContactById
@@ -21,12 +26,13 @@ router.put(
 
 router.patch(
   "/:contactId/favorite",
+  authenticate,
   isValidId,
   validateBody(schemas.updateFavoriteSchema),
   ctrl.updateContactFavorite
 );
 
 
-router.delete("/:contactId", isValidId, ctrl.deleteContact);
+router.delete("/:contactId", authenticate, isValidId, ctrl.deleteContact);
 
 module.exports = router;
